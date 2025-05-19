efcfbf1308d7b332a6f3356d75e52d1f53bec555
Ruben Fuzaylov <ruben.fuzaylov29@qmail.cuny.edu>
2025-05-19 19:40:10 -0400

    Fix homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 export default function Home() {
-  return <h1>Exam 1 Part 1</h1>
+  return <h1>Exam 1 Part 1</h1>;
+}
   (
     <>
       <h1 className="text-3xl font-bold text-gray-800 mb-2">Final Exam 343</h1>
@@ -7,4 +8,3 @@ export default function Home() {
       <h3 className="text-xl text-gray-600">Spring 2025</h3>
     </>
   );
-}
