2c395475f6dc0f6eba5ebf2c16787f2d8bd30bbc
Ruben Fuzaylov <ruben.fuzaylov29@qmail.cuny.edu>
2025-05-19 18:50:46 -0400

    added styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1>Final Exam 343</h1>
-      <h2>Fuzaylov, Ruben</h2>
-      <h3>Spring 2025</h3>
+      <h1 className="text-3xl font-bold text-gray-800 mb-2">Final Exam 343</h1>
+      <h2 className="text-2xl font-bold text-gray-800 mb-1">Fuzaylov,Ruben</h2>
+      <h3 className="text-xl text-gray-600">Spring 2025</h3>
     
     </>
   );
